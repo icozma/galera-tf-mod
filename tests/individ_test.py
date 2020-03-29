34a2c28a5f22b212b98f9bd1d12dceac9716d7ee
Iulian Cozma <iulian.cozma@gmail.com>
2020-03-29 13:30:41 +0300

    refactored individ tests

--- tests/individ_test.py
+++ tests/individ_test.py
@@ -19,7 +19,7 @@ class Individ_tests(unittest.TestCase):
         def my_fitness( i ):
             return -i['x']**2 + 1 -i['y']**2
 
-        self.pb = Problema(fitness=my_fitness, path_to_evol='./', space=Space)
+        self.pb = Problema(fitness=my_fitness, space=Space)
 
     def test_make_individ(self):
         from src.individ import Individ
@@ -66,19 +66,23 @@ class Individ_tests(unittest.TestCase):
 
     def test_asemnare(self):
         a = 0
-        for dix in range(10):
+        for dix in range(2):
             from src.individ import Individ
             i = Individ(pb=self.pb)
             print('\nindivid: ',str(i))
             j = i.mutatie()
             print(' mutant: ',str(j))
-            print('asemanarea: ', str(i.asemanare(j)))
-            a += i.asemanare(j)
+            print('asemanarea: ', str(i.like(j)))
+            a += i.like(j)
            
         self.assertNotEqual(a, 0, "Shall be >0, macar o mutatie ")
 
 
-    def test_from_carcter(self):
+    def test_from_carcter_John(self):
+        """ John has same problem as a boy and as a man
+            John's character is tranfered from he's childhood to manhood
+            are John-s man genes same as John-s boy genes? Should be yes/0
+        """
         import numpy as np
         import json
         from src.problema import Problema
@@ -88,56 +92,43 @@ class Individ_tests(unittest.TestCase):
                     'num_leaves':       [ x for x in np.arange(   30, 50,   1 ) ],
                     'min_child_weight': [ x for x in np.arange( .01, 1,   .01 ) ],
                     'feature_fraction': [ x for x in np.arange( .01, 1,   .01 ) ],
-                    'bagging_fraction': [ x for x in np.arange( .01, 1,   .01 ) ],
-                    'min_data_in_leaf': [ x for x in np.arange(  1, 100,   5  ) ],          
-                    'max_depth':        [ x for x in np.arange(  6, 16,    1  ) ],
+                    # 'bagging_fraction': [ x for x in np.arange( .01, 1,   .01 ) ],
+                    # 'min_data_in_leaf': [ x for x in np.arange(  1, 100,   5  ) ],          
+                    # 'max_depth':        [ x for x in np.arange(  6, 16,    1  ) ],
                     
-                    'bagging_seed':     [ x for x in np.arange(  1, 100,    10  ) ],
+                    # 'bagging_seed':     [ x for x in np.arange(  1, 100,    10  ) ],
 
-                    'reg_alpha':  [ 1e-5, 1e-2, 0.1, 1, 100 ], # [ x for x in np.arange( .1, 100,    .1  ) ],
-                    'reg_lambda': [ x for x in np.arange( .1, 1,    .05  ) ],
-                    'subsample':  [ x for x in np.arange( .1, 1,    .05  ) ],
+                    # 'reg_alpha':  [ 1e-5, 1e-2, 0.1, 1, 100 ], # [ x for x in np.arange( .1, 100,    .1  ) ],
+                    # 'reg_lambda': [ x for x in np.arange( .1, 1,    .05  ) ],
+                    # 'subsample':  [ x for x in np.arange( .1, 1,    .05  ) ],
 
-                    'random_state':   [50],
-                    'boosting_type':  ['gbdt'],
-                    'device':         ['gpu'],
-                    # 'gpu_platform_id': [0],
-                    # 'gpu_device_id':   [0],
+                    # 'random_state':   [50],
+                    # 'boosting_type':  ['gbdt'],
+                    # 'device':         ['gpu'],
+                    # # 'gpu_platform_id': [0],
+                    # # 'gpu_device_id':   [0],
                     
-                    'objective':  ['binary'],
-                    'metric':     ['auc'],       
-                    'verbosity':  [-1],
-                    'n_jobs':     [-1],
-                    'silent':     [False],
-                    'verbose':    [1],
-                    # 'early_stopping_rounds': [100],
-                
-                'learning_rate': [0.3],
-                'n_estimators':  [1000],
+                    # 'objective':  ['binary'],
+                    # 'metric':     ['auc'],       
+                    # 'verbosity':  [-1],
+                    # 'n_jobs':     [-1],
+                    # 'silent':     [False],
+                    # 'verbose':    [1],
+                    # # 'early_stopping_rounds': [100],
+                    
+                    # 'learning_rate': [0.3],
+                    # 'n_estimators':  [1000],
                 }
         
         my_fitness = lambda i: 0
-        Johns_pb = Problema(fitness=my_fitness, path_to_evol='./temp/', space=Space)
-        John = Individ(pb=Johns_pb)
-        s = "{\"num_leaves\": 47, \"min_child_weight\": 0.49, \"feature_fraction\": 0.62, \"bagging_fraction\": 0.86, \"min_data_in_leaf\": 61, \"max_depth\": 15, \"bagging_seed\": 11, \"reg_alpha\": 100, \"reg_lambda\": 0.45000000000000007, \"subsample\": 0.5500000000000002, \"random_state\": 50, \"boosting_type\": \"gbdt\", \"device\": \"gpu\", \"objective\": \"binary\", \"metric\": \"auc\", \"verbosity\": -1, \"n_jobs\": -1, \"silent\": false, \"verbose\": 1, \"learning_rate\": 0.3, \"n_estimators\": 1000}"
-        caracterul_lui_Johny_as_a_boy = json.loads(s)
-       
-        John.from_caracter( caracterul_lui_Johny_as_a_boy )
+        Johns_pb = Problema(fitness=my_fitness, space=Space)
+        John_boy = Individ(pb=Johns_pb)
+        John_man = Individ( pb=Johns_pb, d=John_boy.get_caracter(), verbose=True )
 
-        d = json.loads(s)
-        d['name'] = 'test'
-        jc =  John.get_caracter()['caracter']
-        jc['name'] = 'test'
-        # print(d)
-        # print( jc== d )
-        # print(jc)
-
-        print("\n\nA: ", jc)
-        print("\n\nB:" ,  d)
-        
-
-
-        self.assertEqual(d, jc, "Shall be >0, macar o mutatie ")
+        print( "John boy:\n", John_boy, "\nJohn man:\n", John_man)
+        x = John_boy.like( John_man )
+        print( "John boy like John man: ", x)
+        self.assertEqual(x, 0, "Shall be 0 ")
 
 
 if __name__ == '__main__':
